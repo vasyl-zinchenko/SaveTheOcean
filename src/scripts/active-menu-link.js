917615f15e43579f9affce28c2efe08e92de35a1
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-12-06 11:15:46 +0200

    adding swiper

---
+++ src/scripts/active-menu-link.js
@@ -0,0 +1,9 @@
+/* Change checked active menu-link */
+'use strict';
+
+const menuLink = [...document.querySelectorAll('.menu__link')];
+
+menuLink.map(a => a.addEventListener('click', function() {
+  menuLink.map(b => b.classList.remove('menu__link--is-active'));
+  a.classList.add('menu__link--is-active');
+}));
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,46 @@
-'use strict';
+/* eslint-disable no-unused-vars */
+// import * as preventSubmit from './prevent-submit';
+// import * as scrollUpArrow from './scroll-up';
+// import * as activeLink from './active-menu-link';
+// import * as matchMedia from './match-media';
+// import * as featureSlider from './feature-slider';
+
+// preventSubmit();
+// scrollUpArrow();
+// activeLink();
+// matchMedia();
+// featureSlider();
+
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     document.body.classList.add('page__body--with-menu');
+//   } else {
+//     document.body.classList.remove('page__body--with-menu');
+//   }
+// });
+import Swiper from 'swiper';
+// import Swiper styles
+
+const swiper = new Swiper('.swiper');
+
+//  new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
